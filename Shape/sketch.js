3848aeab44c88dcc908790d40ba2305e489b03be
Brian Sanderlin <briansander@BrianSanMacBook.wifi.pcc.edu>
2020-01-21 11:59:10 -0800

    bezieredits

--- Shape/sketch.js
+++ Shape/sketch.js
@@ -10,9 +10,23 @@ background(0);
 stroke(255);
 noFill();
  beginShape();
-  vertex(mouseX, 300);
-  vertex(500,300);
-  quadraticVertex(433, 17, 373, 322);
-  bezierVertex(141, 104, 266, 201, 122, 299);
+  triangle(100,200,200,100,100,100)
+endShape();
+beginShape();
+  triangle(130,230,230,130,130,130)
+endShape();
+beginShape();
+  triangle(500,400,400,500,500,500)
+endShape();
+beginShape();
+  triangle(470,370,370,470,470,470)
+endShape();
+beginShape();
+vertex(190,190);
+bezierVertex(80, 0, 380, 75, 75, 75);
+vertex(410,410)
+bezierVertex(160, 0, 380, 150, 150, 150);
+vertex(410,410)
+bezierVertex(240, 0, 380, 225, 225, 225);
 endShape();
 }
